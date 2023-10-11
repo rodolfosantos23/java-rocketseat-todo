1d5998681611cf4fdaf3467ba24ab5200c750bc7
Rodolfo Santos <rodolfosantos23@gmail.com>
2023-10-11 11:10:16 -0300

    Class 3 - Password Hash

--- src/main/java/br/com/rodolfosantos/todolist/user/UserController.java
+++ src/main/java/br/com/rodolfosantos/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.rodolfosantos.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,9 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.badRequest().body("User already exists");
         }
+
+        var hashedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(hashedPassword);
 
         var createdUser = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
