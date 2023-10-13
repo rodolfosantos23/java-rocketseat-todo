a40fede579e5d06f9a1239b7aa70e99caa7ccde2
Rodolfo Santos <rodolfosantos23@gmail.com>
2023-10-13 17:29:00 -0300

    Class 4 - Update - Partial Properties

--- src/main/java/br/com/rodolfosantos/todolist/task/TaskController.java
+++ src/main/java/br/com/rodolfosantos/todolist/task/TaskController.java
@@ -1,16 +1,16 @@
 package br.com.rodolfosantos.todolist.task;
 
+import br.com.rodolfosantos.todolist.Utils;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.config.Task;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -35,5 +35,26 @@ public class TaskController {
 
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpServletResponse.SC_CREATED).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request, HttpServletResponse response) {
+        return this.taskRepository.findByUserId((UUID) request.getAttribute("userId"));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        var task = this.taskRepository.findById(id);
+        if (task.isEmpty()) {
+            return ResponseEntity.status(HttpServletResponse.SC_NOT_FOUND).body("Task not found");
+        }
+
+        if (!task.get().getUserId().equals(request.getAttribute("userId"))) {
+            return ResponseEntity.status(HttpServletResponse.SC_FORBIDDEN).body("You don't have permission to update this task");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task.get());
+        this.taskRepository.save(task.get());
+        return ResponseEntity.status(200).body(task);
     }
 }
