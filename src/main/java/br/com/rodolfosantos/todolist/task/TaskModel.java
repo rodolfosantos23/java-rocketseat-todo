e57db9ef8d252db1e8078eb419cb54e9e895ed39
Rodolfo Santos <rodolfosantos23@gmail.com>
2023-10-16 13:16:33 -0300

    Class 5 - Exception Handler

--- src/main/java/br/com/rodolfosantos/todolist/task/TaskModel.java
+++ src/main/java/br/com/rodolfosantos/todolist/task/TaskModel.java
@@ -34,4 +34,11 @@ public class TaskModel {
     private LocalDateTime createdAt;
 
     private UUID userId;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("Title must be less than 50 characters");
+        }
+        this.title = title;
+    }
 }
